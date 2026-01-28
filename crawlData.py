108e57f817ba8f549a3c1e3db3385970378260c6
Benjamin <12860358+userx14@users.noreply.github.com>
2026-01-28 02:02:38 +0100

    switch project to xmltodict

--- crawlData.py
+++ crawlData.py
@@ -1,98 +1,170 @@
-from triasLib import Trias
-from datetime import datetime
+from datetime import datetime, timezone
+import xmltodict
+import requests
 from pathlib import Path
-import xml.etree.ElementTree as ET
-url          = "https://efa-bw.de/trias"
-requestorKey = open("requestor.key").read()
-def stopPointReference_from_name(locationName):
-    findStationRequestXml = Path("LocationInformationRequest_findTrainStations.xml")
-    findStationRequestXml = open(findStationRequestXml, "rb").read()
-    
-    findStationRequest = Trias.from_xml(findStationRequestXml)
-    findStationRequest.service_request.request_timestamp = datetime.now()
-    findStationRequest.service_request.requestor_ref     = requestorKey
-    locationInfoRequest = findStationRequest.service_request.request_payload.location_information_request
-    locationInfoRequest.initial_input.location_name      = locationName
-    locationInfoRequest.location_param_structure.number_of_results = 5
-    
-    print(locationInfoRequest)
-    
-    findStationResponse = findStationRequest.query(url)
-    locationInfoResponse = findStationResponse.service_delivery.delivery_payload.location_information_response
-    for locationResult in locationInfoResponse.location_results_list:
-        print(locationResult.location.stop_point.stop_point_ref)
-        print(locationResult.location.stop_point.stop_point_name.text)
-        
-stopPointReference_from_name("Stuttgart (tief)")
-#stopPointReference_from_name("Stuttgart HBF (tief)") todo fix, is currently broken
 
-"""
+url = "https://efa-bw.de/trias"
+requestorKey = open(Path("./requestor.key")).read()
+namespaces = {
+    "http://www.vdv.de/trias": None,
+    "http://www.siri.org.uk/siri": "siri",
+    "http://www.w3.org/2001/XMLSchema-instance": "xsi"
+}
+requestHeader = {'Content-Type': 'application/xml; charset=utf-8', 'User-Agent': 'Python-urllib/3.10'}
+
+def sendRequest(requestAsDict):
+    serviceRequest   = requestAsDict["Trias"]["ServiceRequest"]
+    currentTimestamp = datetime.now().astimezone(timezone.utc)
+    currentTimestamp = currentTimestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
+    serviceRequest["siri:RequestTimestamp"] = currentTimestamp
+    serviceRequest["siri:RequestorRef"]     = requestorKey
+
+    requestAsXml = xmltodict.unparse(requestAsDict, pretty=True)
+    response = requests.post(url, data=requestAsXml, headers=requestHeader)
+    return xmltodict.parse(response.content, process_namespaces=True, namespaces=namespaces)
+
+def printResponseStatistics(responseDict):
+    serviceDelivery   = responseDict["Trias"]["ServiceDelivery"]
+    responseTimestamp = datetime.strptime(serviceDelivery["siri:ResponseTimestamp"], "%Y-%m-%dT%H:%M:%SZ")
+    responseTimestamp = responseTimestamp.replace(tzinfo=timezone.utc)
+    responseTimestamp = responseTimestamp.astimezone() #convert to local timezone
+    print(f"Response timestamp: {responseTimestamp}")
+
+    calculationTime = serviceDelivery["CalcTime"]
+    print(f"Calculation time: {calculationTime}")
+
 def stopPointRef_from_LocationName(LocationName):
-    xmlTx    = location_information_request(LocationName, NumberOfResults=5)
-    response = requests.post(url, data=xmlTx.encode('utf-8'), headers=requestHeader)
-    root     = ET.XML(response.content)
-    delivery_payload = root.find(".//trias:DeliveryPayload", responseNamespace)
-    LocationInformationResponse = delivery_payload.find("trias:LocationInformationResponse", responseNamespace)
-    ET.indent(delivery_payload)
-   #print(ET.tostring(delivery_payload, encoding='unicode'))
+    findStationRequestXml = Path("./LocationInformationRequest.xml")
+    findStationRequestXml = open(findStationRequestXml, "rb").read()
+    findStationRequest = xmltodict.parse(findStationRequestXml, process_namespaces=True, namespaces=namespaces)
+
+    serviceRequest = findStationRequest["Trias"]["ServiceRequest"]
+    serviceRequest["RequestPayload"]["LocationInformationRequest"]["InitialInput"]["LocationName"] = LocationName
+
+    findStationResponse = sendRequest(findStationRequest)
+    printResponseStatistics(findStationResponse)
+
+    serviceDelivery = findStationResponse["Trias"]["ServiceDelivery"]
+    locationResultsList = serviceDelivery["DeliveryPayload"]["LocationInformationResponse"]["LocationResult"]
+    if not isinstance(locationResultsList, list):
+        locationResultsList = [locationResultsList]
     validStops = []
-    for LocationResult in LocationInformationResponse:
-        #filter for suburban railway stops
-        ModeList = LocationResult.findall("trias:Mode", responseNamespace)
-        for Mode in ModeList:
-            railSubmode = Mode.find("trias:RailSubmode", responseNamespace)
-            if(railSubmode is not None and railSubmode.text == "suburbanRailway"):
+    for locationResult in locationResultsList:
+        transportModesList = locationResult["Mode"]
+        if not isinstance(transportModesList, list):
+            transportModesList = [transportModesList]
+        for transportModes in transportModesList:
+            if(transportModes["PtMode"] == "rail") and (transportModes["RailSubmode"] == "suburbanRailway"):
                 break
         else:
-            continue #skip stop if it does not match filter
-        #extract information stopname and stopref
-        StopPoint = LocationResult.find(".//trias:StopPoint", responseNamespace)
-        ref       = StopPoint.find("trias:StopPointRef", responseNamespace).text
-        name      = StopPoint.find("trias:StopPointName", responseNamespace)
-        name      = name.find("trias:Text", responseNamespace).text
+            continue
+        ref  = locationResult["Location"]["StopPoint"]["StopPointRef"]
+        name = locationResult["Location"]["StopPoint"]["StopPointName"]["Text"]
         validStops.append((name, ref))
     print(validStops)
     return validStops[0][1]
 
+def tripRef_from_locations(originRef, destinationRef, arrivalTime: datetime):
+    tripRequestXml = Path("./TripRequest.xml")
+    tripRequestXml = open(tripRequestXml, "rb").read()
+    tripRequest = xmltodict.parse(tripRequestXml, process_namespaces=True, namespaces=namespaces)
 
-def tripsStammstrecke(time):
-    stgHbfTiefRef    = stopPointRef_from_LocationName("Stuttgart (tief)")
-    stgStadtmitteRef = stopPointRef_from_LocationName("Stuttgart Stadtmitte")
-    xmlTx            = trip_request(stgHbfTiefRef, time, stgStadtmitteRef)
-    response         = requests.post(url, data=xmlTx, headers=requestHeader)
-    root             = ET.XML(response.content)
-    delivery_payload = root.find(".//trias:DeliveryPayload", responseNamespace)
-    ET.indent(root)
-    print(ET.tostring(root, encoding='unicode'))
+    serviceRequest = tripRequest["Trias"]["ServiceRequest"]
+    serviceRequest["RequestPayload"]["TripRequest"]["Origin"]["LocationRef"]["StopPointRef"] = originRef
+    datetimestring = arrivalTime.astimezone(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+    serviceRequest["RequestPayload"]["TripRequest"]["Origin"]["DepArrTime"] = datetimestring
+    serviceRequest["RequestPayload"]["TripRequest"]["Destination"]["LocationRef"]["StopPointRef"] = destinationRef
+    serviceRequest["RequestPayload"]["TripRequest"]["Params"]["NumberOfResults"] = 50
 
-tripsStammstrecke(datetime.now())
+    tripResponse = sendRequest(tripRequest)
+    printResponseStatistics(tripResponse)
 
+    serviceDelivery = tripResponse["Trias"]["ServiceDelivery"]
+    for tripResult in serviceDelivery["DeliveryPayload"]["TripResponse"]["TripResult"]:
+        operatingDayRef = tripResult["Trip"]["TripLeg"]["TimedLeg"]["Service"]["OperatingDayRef"]
+        journeyRef = tripResult["Trip"]["TripLeg"]["TimedLeg"]["Service"]["JourneyRef"]
+        print(f"operatingDayRef: {journeyRef}: {operatingDayRef}")
 
-#xml = trip_information_request("ddb:90T10:B:H", datetime.now())
-#xml = trip_request("test", datetime.now(), "test2")
-xml = location_information_request("Stuttgart hbf (tief)")
-print(xml)
+def getTripInformation(tripRef, operatingDayRef):
+    tripInfoRequestXml = Path("./TripInformationRequest.xml")
+    tripInfoRequestXml = open(tripInfoRequestXml, "rb").read()
+    tripInfoRequest = xmltodict.parse(tripInfoRequestXml, process_namespaces=True, namespaces=namespaces)
 
-#print(response.status_code, response.text, len(xml))
+    serviceRequest = tripInfoRequest["Trias"]["ServiceRequest"]
+    serviceRequest["RequestPayload"]["TripInfoRequest"]["JourneyRef"] = tripRef
+    datetimestring = operatingDayRef.astimezone(timezone.utc).strftime("%Y-%m-%dT")
+    serviceRequest["RequestPayload"]["TripInfoRequest"]["OperatingDayRef"] = datetimestring
+    serviceRequest["RequestPayload"]["TripInfoRequest"]["Params"]["IncludePosition"] = "true"
 
-print(response.text)
-root = ET.XML(response.text)
-ET.indent(delivery_payload)
-pretty = ET.tostring(delivery_payload, encoding='unicode')
-print(pretty)
-with open("out.txt", "w", encoding="utf-8") as outfile:
-    outfile.write(pretty)
-
-
-trias.key("")
-trias.url("")
+    tripInfoResponse = sendRequest(tripInfoRequest)
+    printResponseStatistics(tripInfoResponse)
 
 
-req = trias.StopEventRequest()
-req["StopEventRequest","Location"]
-req[0,"test"]
-req.StopEventRequest.Location.LocationRef.StopPointRef   = "de:08111:6118"
-req.StopEventRequest.Location.Params.IncludeRealtimeData = True
-response = trias.send(req)
-response.trip
+    serviceDelivery = tripInfoResponse["Trias"]["ServiceDelivery"]
+    if("ErrorMessage" in serviceDelivery["DeliveryPayload"]["TripInfoResponse"].keys()):
+        print(serviceDelivery["DeliveryPayload"]["TripInfoResponse"]["ErrorMessage"]["Code"])
+        return
+    tripInfoResult = serviceDelivery["DeliveryPayload"]["TripInfoResponse"]["TripInfoResult"]
+    sbahnLineName = tripInfoResult["Service"]["ServiceSection"]["PublishedLineName"]["Text"]
+    sbahnDestinationName = tripInfoResult["Service"]["DestinationText"]["Text"]
+    
+    print(f"{sbahnLineName} to {sbahnDestinationName}")
+    for previousCall in tripInfoResult["PreviousCall"]:
+        stopName      = previousCall["StopPointName"]["Text"]
+
+        if "ServiceDeparture" in previousCall.keys():
+            departureOrArrival = previousCall["ServiceDeparture"]
+        elif "ServiceArrival" in previousCall.keys():
+            departureOrArrival = previousCall["ServiceArrival"]
+        else:
+            raise ValueError("No departure or arrival found")
+
+        timetableTime = departureOrArrival["TimetabledTime"]
+        timetableTime = datetime.strptime(timetableTime, "%Y-%m-%dT%H:%M:%SZ")
+        timetableTime = timetableTime.replace(tzinfo=timezone.utc)
+        timetableTime = timetableTime.astimezone()
+
+        estimatedTime = departureOrArrival["EstimatedTime"]
+        estimatedTime = datetime.strptime(estimatedTime, "%Y-%m-%dT%H:%M:%SZ")
+        estimatedTime = estimatedTime.replace(tzinfo=timezone.utc)
+        estimatedTime = estimatedTime.astimezone()
+        delay = (estimatedTime - timetableTime).total_seconds() / 60
+        print(f"{stopName}: {delay}")
+    print("<---  current ")
+    if "OnwardCall" in tripInfoResult.keys():
+        for onwardCall in tripInfoResult["OnwardCall"]:
+            stopName      = onwardCall["StopPointName"]["Text"]
+
+            if "ServiceDeparture" in onwardCall.keys():
+                departureOrArrival = onwardCall["ServiceDeparture"]
+            elif "ServiceArrival" in onwardCall.keys():
+                departureOrArrival = onwardCall["ServiceArrival"]
+            else:
+                raise ValueError("No departure or arrival found")
+
+            timetableTime = departureOrArrival["TimetabledTime"]
+            timetableTime = datetime.strptime(timetableTime, "%Y-%m-%dT%H:%M:%SZ")
+            timetableTime = timetableTime.replace(tzinfo=timezone.utc)
+            timetableTime = timetableTime.astimezone()
+
+            estimatedTime = departureOrArrival["EstimatedTime"]
+            estimatedTime = datetime.strptime(estimatedTime, "%Y-%m-%dT%H:%M:%SZ")
+            estimatedTime = estimatedTime.replace(tzinfo=timezone.utc)
+            estimatedTime = estimatedTime.astimezone()
+            delay = (estimatedTime - timetableTime).total_seconds() / 60
+            
+            print(f"{stopName}: {delay}")
+
+    print(tripInfoResult["Service"])
+    #print(serviceDelivery)
+
+
+hauptbahnhofRef = "de:08111:6118"#stopPointRef_from_LocationName("Stuttgart (tief)")
+stadtmitteRef   = "de:08111:6052"#stopPointRef_from_LocationName("Stuttgart Schwabstraße")
+
+#tripRef_from_locations(hauptbahnhofRef, stadtmitteRef, datetime.now())
+getTripInformation(f"ddb:92T03::H:j26:612", datetime.now())
+"""
+for i in range(10):
+    
 """
