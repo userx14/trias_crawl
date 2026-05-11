62ce5cf6f771968249bef9e453c00dfc8e460a47
Benjamin <12860358+userx14@users.noreply.github.com>
2026-05-11 02:38:42 +0200

    json api testing

--- jsonApi.py
+++ jsonApi.py
@@ -2,7 +2,7 @@ import json
 import requests
 from pathlib import Path
 import xmltodict
-
+from datetime import datetime
 
 base_dir = Path(__file__).parent
 url = open(base_dir/"jsonApi.key").read().replace("\n", "").replace("\r","")
@@ -15,6 +15,7 @@ def stopFinderRequest(stopName, stopType="stop"):
         "type_sf": stopType,
     }
     response = requests.get(url+"XML_STOPFINDER_REQUEST", params = payload, headers=requestHeader)
+    #jsonPayload["itdRequest"]["itdStopFinderRequest"]["itdOdv"]
     return xmltodict.parse(response.content)
 
 def servingLinesRequest():
@@ -36,9 +37,31 @@ def addInfoRequest():
     payload = {
         "operatorCode": "DB",
         "filterDateValid": datetime.today().strftime('%d-%m-%Y'),
+        "filterMOTType": 1
     }
-    requests.get(url+"XML_ADDINFO_REQUEST", params = payload, headers=requestHeader)
-    pass
+    response = requests.get(url+"XML_ADDINFO_REQUEST", params = payload, headers=requestHeader)
+    return xmltodict.parse(response.content)
 
-jsonPayload = stopFinderRequest("Backnang", stopType="any")
-print(jsonPayload["itdRequest"]["itdStopFinderRequest"]["itdOdv"])
+jsonPayload = addInfoRequest()
+#print(jsonPayload)
+for travelInfo in jsonPayload["itdRequest"]["itdAddInfoRequest"]["itdAdditionalTravelInformations"]["itdAdditionalTravelInformation"]:
+    print(travelInfo.keys())
+
+    print("infolink text")
+    print(travelInfo["infoLink"]["infoLinkText"])
+    print("\n")
+
+    print("info text")
+    print(travelInfo["infoLink"]["infoText"])
+    print("\n")
+
+    print("affected lines")
+    print(travelInfo["concernedLines"]['line'])
+    print("\n")
+    #print(travelInfo["concernedStops"])
+print("\n\n")
+#print(jsonPayload["itdRequest"]["itdAddInfoRequest"]["itdUniqueTrainList"])
+#print("\n\n")
+#print(jsonPayload["itdRequest"]["itdAddInfoRequest"]["itdUniqueLineList"])
+#print("\n\n")
+#print(jsonPayload["itdRequest"]["itdAddInfoRequest"]["itdUniqueStopList"])
