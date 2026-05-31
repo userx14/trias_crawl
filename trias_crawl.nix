d56349bb8895f6cbb73a707d03609f810f2bca92
Benjamin <12860358+userx14@users.noreply.github.com>
2026-05-31 15:52:38 +0200

    faster update rate

--- trias_crawl.nix
+++ trias_crawl.nix
@@ -5,14 +5,15 @@ let
     numpy
     svgpathtools
     xmltodict
+    matplotlib
   ]);
 in {
   
   users.users.pingu2.linger = true;
-  systemd.user.services.trias-crawler = {
-    description = "Trias crawler";
+  systemd.user.services.trias-live-crawler = {
+    description = "trias live crawler";
     serviceConfig = {
-      ExecStart = "${pythonEnv}/bin/python /mnt/data/trias_crawl/main.py";
+      ExecStart = "${pythonEnv}/bin/python /mnt/data/trias_crawl/main.py live";
       Restart = "on-failure";
       RestartSec = 120;
       StandardOutput = "journal";
@@ -21,12 +22,35 @@ in {
     };
   };
 
-  systemd.user.timers.trias-crawler = {
-    description = "run Trias crawler every 5 minutes";
+  systemd.user.timers.trias-live-crawler = {
+    description = "run trias live crawler every minute";
     timerConfig = {
-      OnCalendar = "*:0/5";
+      OnCalendar = "*:0/1";
       Persistent = true;
     };
     wantedBy = [ "timers.target" ];
   };
+
+  systemd.user.services.trias-stat-crawler = {
+    description = "trias stat crawler";
+    serviceConfig = {
+      ExecStart = "${pythonEnv}/bin/python /mnt/data/trias_crawl/main.py stat";
+      Restart = "on-failure";
+      RestartSec = 120;
+      StandardOutput = "journal";
+      StandardError = "journal";
+      Environment = "HOME=/home/pingu2";
+    };
+  };
+
+  systemd.user.timers.trias-stat-crawler = {
+    description = "run trias stat crawler every 30 minutes";
+    timerConfig = {
+      OnCalendar = "*:0/30";
+      Persistent = true;
+    };
+    wantedBy = [ "timers.target" ];
+  };
+
+
 }
